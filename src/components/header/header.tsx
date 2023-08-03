c7f3d3056e1a62f2ff3b6e60734b555c97b940e4
arezhz <arezhz@gmail.com>
2023-08-03 14:17:49 +0330

    header menu

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,23 +1,60 @@
 'use client'
 import styles from './header.module.css'
-import {Button, Flex, Heading, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
-import {AddIcon, ChevronDownIcon} from "@chakra-ui/icons";
+import {
+    Avatar,
+    Button,
+    Flex,
+    Heading,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
+    Text
+} from "@chakra-ui/react";
+import {AddIcon, TriangleDownIcon} from "@chakra-ui/icons";
 
 export default function Header() {
+   const menuButtonFocus = {
+        boxShadow: 'none',
+    }
+    const menuButtonActive = {
+        bg: 'transparent',
+        borderColor: 'transparent',
+    }
+    const menuButtonHover ={bg: 'transparent'}
+
     return (
         <header className={styles.header}>
-            <Flex alignItems="center" justifyContent="space-between">
-                <Heading as='h1'>
+            <Flex alignItems="center"
+                  justifyContent="space-between"
+                  height="100%">
+                <Heading as='h1' noOfLines={1}>
                     لیست سوالات
                 </Heading>
-                <Flex>
+                <Flex alignItems="center"
+                      justifyContent="space-between"
+                      height="100%">
                     <Button leftIcon={<AddIcon/>}
-                            colorScheme='green'>
+                            colorScheme='green'
+                            me={16}>
                         سوال جدید
                     </Button>
                     <Menu>
-                        <MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
-                            Actions
+                        <MenuButton as={Button}
+                                    rightIcon={<TriangleDownIcon/>}
+                                    bg='transparent'
+                                    _hover={menuButtonHover}
+                                    _active={menuButtonActive}
+                                    _focus={menuButtonFocus}>
+                            <Flex alignItems="center"
+                                  justifyContent="space-between">
+                                <Avatar name='Hadi zare'
+                                        w='44px'
+                                        h='44px'
+                                        me={3}
+                                        src='https://bit.ly/tioluwani-kolawole'/>
+                                <Text fontSize='sm'>هادی زارع ایرانی</Text>
+                            </Flex>
                         </MenuButton>
                         <MenuList>
                             <MenuItem>Download</MenuItem>
